c209ca5bb7f66811911c14aba6a4b27c874fc762
sandalu95 <sandalukalpanee@gmail.com>
2018-04-03 14:42:35 +0530

    This is my first ionic project.

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  goToNextPage() : void {
+    this.navCtrl.push('Page2Page');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+}
